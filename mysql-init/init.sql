ddb1db395bff402123f4885b75b84e8d4404fdf8
Omar Franco <hhernandezf1800@alumno.ipn.mx>
2025-04-23 16:57:47 -0600

    A dummy endpoint and catalog for system names were created

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,6 +1,20 @@
--- mysql-init/init.sql
 CREATE DATABASE IF NOT EXISTS monitoreo_pdn;
 USE monitoreo_pdn;
+
+-- Tabla de catálogo de sistemas
+CREATE TABLE IF NOT EXISTS catalogo_sistemas (
+    id INT PRIMARY KEY,
+    codigo VARCHAR(10) NOT NULL,
+    nombre VARCHAR(255) NOT NULL,
+    
+    INDEX idx_codigo (codigo)
+);
+
+-- Inserción de datos en el catálogo de sistemas
+INSERT INTO catalogo_sistemas (id, codigo, nombre) VALUES 
+(1, 'S1', 'Sistema de evolución patrimonial, de declaración de intereses y constancia de presentación de declaración fiscal'),
+(2, 'S2', 'Sistema de los servidores públicos que intervengan en procedimientos de contrataciones públicas'),
+(3, 'S3', 'Sistema nacional de servidores públicos y particulares sancionados');
 
 CREATE TABLE IF NOT EXISTS registros_ejecucion (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -14,9 +28,12 @@ CREATE TABLE IF NOT EXISTS registros_ejecucion (
     
     INDEX idx_sistema_origen (sistema_origen),
     INDEX idx_fecha_ejecucion (fecha_ejecucion),
-    INDEX idx_ente (ente)
+    INDEX idx_ente (ente),
+    
+    CONSTRAINT fk_sistema_origen FOREIGN KEY (sistema_origen) 
+    REFERENCES catalogo_sistemas(codigo) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- Usa el nombre de usuario y contraseña que definiste en tu .env
-GRANT ALL PRIVILEGES ON registros_db.* TO 'registros_user'@'%' IDENTIFIED BY 'password_segura';
+GRANT ALL PRIVILEGES ON monitoreo_pdn.* TO 'registros_user'@'%' IDENTIFIED BY 'password_segura';
 FLUSH PRIVILEGES;
